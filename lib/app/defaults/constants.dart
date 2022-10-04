60f588179dadeb2df5fe97c13ef2dc18a94a242b
Miguel Henrique <miguelhp373@gmail.com>
2022-10-03 22:28:07 -0300

    initial files

---
+++ lib/app/defaults/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//Default Colors
+const primaryColor = Color(0xFF8854d0);
+const secodaryColor = Color(0xFFa55eea);
+const buttonColor = Color(0xFF663399);
+const textPrimaryColor = Color(0xFFFFFFFF);
+const textColorinLightDisplay = Color(0xFF3C4046);
+const textFieldBackground = Color(0xF3F3F3F3);
